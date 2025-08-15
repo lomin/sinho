b530751fa8da71834e15ee94b2352880f31be56a
Steven Collins <happylomin@gmail.com>
2025-08-16 02:25:22 +0900

    Added licence information

--- build.clj
+++ build.clj
@@ -20,7 +20,15 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/lomin/sinho"
+                      :connection "scm:git:git://github.com/lomin/sinho.git"
+                      :developerConnection "scm:git:ssh://git@github.com/lomin/sinho.git"
+                      :tag (str "v" version)}
+                :pom-data [[:licenses
+                            [:license
+                             [:name "Apache License, Version 2.0"]
+                             [:url "http://www.apache.org/licenses/LICENSE-2.0"]]]]})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
@@ -48,9 +56,9 @@
 (defn deploy [opts]
   (jar opts)
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
-    (merge {:installer :remote
-                       :artifact jar-file
-                       :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
-                    opts))
+   (merge {:installer :remote
+           :artifact jar-file
+           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
   opts)
 
